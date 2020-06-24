a39535677f9c64c25f011ae1292e09a8f47f601f
Jorge Ramirez <joramirez96@gmail.com>
2020-06-24 20:51:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
+  attr_accessor :pets, :mood
+  attr_reader :species, :name, :cats, :dogs
   # code goes here
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+    @pets = {:cats => [], :dogs => []}
+    @species = "human"
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    count = 7
+  end
+
 end
