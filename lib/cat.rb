0452b2fc4e1d36e28fa48942594852d4ae9175c7
Jorge Ramirez <joramirez96@gmail.com>
2020-06-24 21:38:10 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -9,13 +9,8 @@ class Cat
   def initialize(name, owner, mood="nervous")
     @name = name
     @owner = owner
-    owner.cats << self
     @mood = mood
     @@cats << self
-  end
-
-  def name
-    @name
   end
 
   def self.all
